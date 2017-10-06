ae0f779f71e417a8094201364a93d993e4f05a2d
Eduard Bakstein <eda@zzz.cz>
2017-10-06 13:07:03 +0200

    multi-platform (win compatible) tmp folder

--- load_compressed_nii.m
+++ load_compressed_nii.m
@@ -13,12 +13,12 @@ function [nifti_out] = load_compressed_nii(image_path)
     % assign a new tmp_id to each volume is useful when the same images are used in parallel
     [folder, f] = fileparts(image_path);
     tmp_id = num2str(round(rand*1000*s(6)));
-    tmppath = fullfile('/','tmp',[f,'_', tmp_id,'.nii.gz']);
+    tmppath = fullfile(tempdir,[f,'_', tmp_id,'.nii.gz']);
     copyfile([image_path,'.nii.gz'], tmppath);
     gunzip(tmppath);
-    nifti_out = load_untouch_nii(fullfile('/','tmp',[f,'_', tmp_id,'.nii']));
-    delete(fullfile('/','tmp',[f,'_', tmp_id,'.nii.gz']));
-    if exist(tmppath, 'file')
+    nifti_out = load_nii(tmppath);
+    delete(tmppath);
+    if exist(tmppath, 'file') % ??
         delete(tmppath);
     end
 end
